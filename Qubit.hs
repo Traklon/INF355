81f08d01dc45be41f5ba2acba7e191da0addc5f5
Arnaud Fraulaud <arnaud.fraulaud@gmail.com>
2014-05-30 12:00:54 +0200

    Addition of 2 more useful Registers and Notation change ("data Qubits = State..." -> "data Register = Qubits...").

--- Qubit.hs
+++ Qubit.hs
@@ -51,9 +51,9 @@ genKets i = reverse (gK i) where
 
 
 
--- Introduction of Qubits.
+-- Introduction of Register.
 --
--- In quantum computing, Bits are replaced with Qubits.
+-- In quantum computing, Bits are replaced with Register.
 -- They can be in state |0> or |1> just like normal Bits, but also
 -- in a linear combination of them, in regard with the complex field.
 --
@@ -66,51 +66,51 @@ genKets i = reverse (gK i) where
 -- I still don't know whether or not I should state if a value is
 -- fixed in the Qubit definition. I will see later.
 --
--- Registers are concatenated Qubits. They can be seen as binary numbers.
+-- Registers are concatenated Register. They can be seen as binary numbers.
 --
--- Now, all the functions are directly done on Registers, called "Qubits".
+-- Now, all the functions are directly done on Registers.
 -- In fact, a single Qubit is a register too.
 -- This makes calculations easier and cleaner.
 --
--- Qubits are represented by a Vector. The value in the index i represent
+-- Registers are represented by a Vector. The value in the index i represent
 -- the possible value of the register i-1.
 -- This is much quicker than with lists, and much more logical.
 -- Furthermore, it interacts well with matrices.
 
-data Qubits = State (V.Vector Complex)
+data Register = Qubits (V.Vector Complex)
 
--- Some useful Qubits
-qZero :: Qubits
-qZero = State $ V.fromList [1, 0]
+-- Some useful Register.
+qZero :: Register
+qZero = Qubits $ V.fromList [1, 0]
 
-qOne :: Qubits
-qOne = State $ V.fromList [0, 1]
+qOne :: Register
+qOne = Qubits $ V.fromList [0, 1]
 
-qMoy :: Qubits
-qMoy = signum $ State $ V.fromList [1,1]
+qMoy :: Register
+qMoy = signum $ Qubits $ V.fromList [1,1]
 
-qInv :: Qubits
-qInv = signum $ State $ V.fromList [1,-1]
+qInv :: Register
+qInv = signum $ Qubits $ V.fromList [1,-1]
 
 -- Pretty printing.
-instance Show Qubits where
-  show (State t) = show' t' where
+instance Show Register where
+  show (Qubits t) = show' t' where
     t' = zip (genKets (V.length t)) (V.toList t)
     show' [] = ""
     show' [(k,c)] = "(" ++ show c ++ ")" ++ (show k)
     show' ((k,c):s) = "(" ++ show c ++ ")" ++ (show k) ++ " + " ++ show' s
 
--- Allows operations between 2 Qubits, useful for the instanciation of Num.
-op :: (Complex -> Complex -> Complex) -> Qubits -> Qubits -> Qubits
-op f (State t) (State u) = State $ V.zipWith f t u
+-- Allows operations between 2 Register, useful for the instanciation of Num.
+op :: (Complex -> Complex -> Complex) -> Register -> Register -> Register
+op f (Qubits t) (Qubits u) = Qubits $ V.zipWith f t u
 
 -- Same with unary operations.
-opU :: (Complex -> Complex) -> Qubits -> Qubits
-opU f (State l) = State $ V.map f l
+opU :: (Complex -> Complex) -> Register -> Register
+opU f (Qubits l) = Qubits $ V.map f l
 
 -- Some useful calculations, some are useless, and are just here for
 -- the sake of completeness.
-instance Num Qubits where
+instance Num Register where
   (+) q1 q2 = op (+) q1 q2
   (-) q1 q2 = op (-) q1 q2
   (*) q1 q2 = op (*) q1 q2
@@ -118,25 +118,35 @@ instance Num Qubits where
   abs q = qZero                                     -- Totally useless.
 
   signum q = opU (mDC (1/(sqrt d))) q               -- Awesomely useful, since the "direction" of
-    where State l = opU abs $ op (*) q q            -- Qubits is all that matters.
+    where Qubits l = opU abs $ op (*) q q           -- Register is all that matters.
           Comp d _ = V.sum l                        -- It's like a "normalize" function.
                                                     
   fromInteger i = qZero                             -- Useless.
 
+-- Generates a Register with 0 everywhere except in i.
+-- The register has size the smallest power of 2 higher (or equal) than i.
+qOneI :: Double -> Register 
+qOneI i = Qubits $ V.fromList [if (x == i) then 1 else 0 | x <- [0..(c-1)]] 
+  where c = max 2 $ 2^(ceiling ((log (i+1))/(log 2)))
 
--- A Gate is a function applied to a Register (Qubits).
+-- Generates a Register of size n, with every probability being the same,
+-- the sum of their squares being 1.
+qEq :: Int -> Register
+qEq n = signum $ Qubits $ V.fromList $ replicate n 1
+
+-- A Gate is a function applied to a Register (Register).
 -- A Gate is homeomorphic to a matrix of complex numbers,
 -- in the {|0>, |1>...} basis.
-data Gate = G (Qubits -> Qubits)
+data Gate = G (Register -> Register)
 
 -- Allows a Register to be an argument of a matrix, seen as a function (like Gates).
-mQM :: (M.Matrix Complex) -> Qubits -> Qubits
-mQM m q = State $ V.fromList [V.sum (V.zipWith (*) (M.getRow i m) v) | i <- [1.. (V.length v)]]
-  where State v = q
+mQM :: (M.Matrix Complex) -> Register -> Register
+mQM m q = Qubits $ V.fromList [V.sum (V.zipWith (*) (M.getRow i m) v) | i <- [1.. (V.length v)]]
+  where Qubits v = q
 
--- Transforms a list of Qubits into a matrix, in ordre to construct Gates.
-qToM :: [Qubits] -> (M.Matrix Complex)
-qToM l = foldr (M.<->) (M.matrix 0 (length l) (\_ -> 0)) [M.rowVector x | (State x) <- l]
+-- Transforms a list of Register into a matrix, in ordre to construct Gates.
+qToM :: [Register] -> (M.Matrix Complex)
+qToM l = foldr (M.<->) (M.matrix 0 (length l) (\_ -> 0)) [M.rowVector x | (Qubits x) <- l]
 
 -- hadamard is a gate that transforms |0> into (|0> + |1>) / (sqrt 2)
 --                                and |1> into (|0> - |1>) / (sqrt 2)
@@ -153,4 +163,3 @@ shift theta = G (mQM m) where
 
 -- TODO : Séparer le code en plusieurs fichiers.
 --        Autres portes.
---        Ecrire fonctions pour créer des registres de base (0 partout, 1 quelque part ; équilibré...)
